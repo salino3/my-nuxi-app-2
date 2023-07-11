2b6feeed0fd850b103d9d13f187586c0a7d30e6f
salino3 <flaviobcn012@gmail.com>
2023-07-11 16:11:16 +0200

    first commit

---
+++ pages/users/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>A single user with ID: {{ $route.params.id  ? $route.params.id  : "Unknown" }}</h1>
+  
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scope>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>This is the User page</h1>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scope>
+
+</style>
